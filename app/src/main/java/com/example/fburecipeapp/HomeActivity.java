ffe1ec83a45fd90e582e859fc6c2d3f054dca530
ChrisMunene <christopherkinyua@college.harvard.edu>
2019-07-15 16:34:35 -0700

    Scanner - Migrate to Androidx

--- app/src/main/java/com/example/fburecipeapp/HomeActivity.java
+++ app/src/main/java/com/example/fburecipeapp/HomeActivity.java
@@ -1,13 +1,12 @@
 package com.example.fburecipeapp;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.fburecipeapp.fragments.ScannerFragment;
