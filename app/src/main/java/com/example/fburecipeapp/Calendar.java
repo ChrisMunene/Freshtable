f17f4f682ea1b8cd952fd5d1ffc282593fbde7c9
khushic7 <khushicm7@gmail.com>
2019-07-16 10:56:20 -0700

    initial calendarview

--- app/src/main/java/com/example/fburecipeapp/Calendar.java
+++ app/src/main/java/com/example/fburecipeapp/Calendar.java
@@ -1,8 +1,12 @@
 package com.example.fburecipeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class Calendar extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class Calendar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(getApplicationContext(), ""+dayOfMonth, 0).show();
+            }
+        });
+
     }
 }
